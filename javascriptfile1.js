acabc871f2019c5bfefd70c7e3289f40e7bd5738
Divyansh Bahubal <747855@student.tor.loc>
2019-06-26 15:01:43 -0400

    3.00pm commit

--- javascriptfile1.js
+++ javascriptfile1.js
@@ -1,5 +1,10 @@
 function waitingFn(timeInMs) {
     const futureTime = Date.now() + timeInMs;
+    while (futureTime > Date.now()) {
+
+    }
+
 }
+debugger;
 waitingFn(3000);
 console.log("function call just has ended");
